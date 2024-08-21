29e11761b7ddb853f295426453c97d81e95f3378
Adam Parsons <aparsons@users.noreply.github.com>
2024-08-21 16:26:19 -0500

    Set output directory back to out

--- resume-app/next.config.mjs
+++ resume-app/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export", // Enable static export
-    distDir: 'dist', // Change the output directory `out` -> `dist`
 };
 
 export default nextConfig;

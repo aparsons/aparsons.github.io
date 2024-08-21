20ebc38bab30247c00013f27123851675e73ce48
Adam Parsons <aparsons@users.noreply.github.com>
2024-08-21 15:54:12 -0500

    use dist folder

--- resume-app/next.config.mjs
+++ resume-app/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export", // Enable static export
+    distDir: 'dist', // Change the output directory `out` -> `dist`
 };
 
 export default nextConfig;

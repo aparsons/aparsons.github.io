35926b51f59a89ca2d9e6e2a3a86fa5311b1d7d0
Adam Parsons <aparsons@users.noreply.github.com>
2024-09-04 18:17:29 -0500

    Update next.config.mjs

--- resume-app/next.config.mjs
+++ resume-app/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export", // Enable static export
+    webpack: (config, context) => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300
+        }
+        return config
+    }
 };
 
 export default nextConfig;

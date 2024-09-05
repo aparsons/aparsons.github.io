bd42ae5310be28d5577b66639c20091b2af38e43
Adam Parsons <aparsons@users.noreply.github.com>
2024-09-04 19:17:50 -0500

    Adding some basic styling

--- resume-app/src/app/page.tsx
+++ resume-app/src/app/page.tsx
@@ -1,14 +1,34 @@
 export default function Home() {
   return (
     <main>
-      <h1>Adam Parsons</h1>
-      <p>
-        <a href="https://github.com/aparsons/aparsons.github.io">Github</a>
-      </p>
-      <div className="flex flex-row">
-        <div className="basis-1/4">01</div>
-        <div className="basis-1/4">02</div>
-        <div className="basis-1/2">03</div>
+      <div className="my-6 p-20 container mx-auto bg-white border-2 border-current shadow-2xl">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Adam Parsons</h1>
+          <p>adam@parsons.cloud - <a href="https://github.com/aparsons">github.com/aparsons</a></p>
+          <h2 className="mt-6 mb-3 text-3xl font-bold">WEB APPLICATION SECURITY PROFESSIONAL</h2>
+          <p className="text-left">Analytical and detail-oriented web application security professional with a passion for information technology and security. Highly adept at web design and development. Often bridges the gap between business and technology groups. Excellent presentation and communication skills. Core knowledge and skill areas:</p>
+          <div className="mx-20 my-3 text-left">
+            <div className="flex flex-row">
+              <div className="basis-1/2">Security Testing Automation</div>
+              <div className="basis-1/2">Web Application Penetration Testing</div>
+            </div>
+            <div className="flex flex-row">
+              <div className="basis-1/2">Software Engineering and Development</div>
+              <div className="basis-1/2">On-Site Training</div>
+            </div>
+            <div className="flex flex-row">
+              <div className="basis-1/2">Threat Analysis and Risk Management</div>
+              <div className="basis-1/2">Policy Development</div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+      <div className="mb-6 container mx-auto">
+        <div className="text-center text-sm text-slate-600">
+          <a href="https://github.com/aparsons/aparsons.github.io">View the code for this page</a>
+        </div>
       </div>
     </main>
   );
